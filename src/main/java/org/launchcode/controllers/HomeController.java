dd53edf4a79041500490211f09a1deddffa25aae
rarose67 <robertrosestl67@gmail.com>
2018-05-23 20:20:35 -0500

    Bonus mission: Made actionChoices list be generated by the views.

--- src/main/java/org/launchcode/controllers/HomeController.java
+++ src/main/java/org/launchcode/controllers/HomeController.java
@@ -1,7 +1,6 @@
 package org.launchcode.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
@@ -10,16 +9,10 @@ import java.util.HashMap;
  * Created by LaunchCode
  */
 @Controller
-public class HomeController {
+public class HomeController extends TechJobsController {
 
     @RequestMapping(value = "")
-    public String index(Model model) {
-
-        HashMap<String, String> actionChoices = new HashMap<>();
-        actionChoices.put("search", "Search");
-        actionChoices.put("list", "List");
-
-        model.addAttribute("actions", actionChoices);
+    public String index() {
 
         return "index";
     }
